25b85944339f21b54140aaa40374401ea036802c
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2021-01-21 11:20:34 -0600

    Add functions to retrieve data and package files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pitch_predictor',
+        version='0.0',
+        description='Predict pitches with pybaseball',
+        url='https://github.com/rmatsum836/pitch_predictor',
+        author='Ray Matsumoto',
+        author_email='raymatsum@gmail.com',
+        license='MIT',
+        packages=['pitch_predictor'],
+        zip_safe=False)
